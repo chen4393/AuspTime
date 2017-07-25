0b94058196895a53ba4d2ab6df69923b0a920eb5
Chaoran Chen <chen4393@umn.edu>
2017-07-25 16:56:34 -0500

    first commit

---
+++ AuspTime/AuspTime/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AuspTime
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            double padding = 0.0;
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    padding = 20;
+                    break;
+                case Device.Android:
+                    padding = 0;
+                    break;
+            }
+            this.Padding = new Thickness(0, padding, 0, 0);
+        }
+    }
+}
